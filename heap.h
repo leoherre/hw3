d5962e93ed4a18863e46ae6b7a228888b1415dd4
Leonel Herrera <leoherre@usc.edu>
2022-10-20 23:53:29 +0000

    All done

--- heap.h
+++ heap.h
@@ -2,6 +2,7 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
 
 template <typename T, typename PComparator = std::less<T> >
 class Heap
@@ -60,16 +61,85 @@ public:
   size_t size() const;
 
 private:
-  /// Add whatever helper functions and data members you need below
-
-
-
-
+  std::vector<T> heapUsed;
+  int mAry;
+  PComparator comp;
+  void heapify(int indx);
+  void trickleUp(int loc);
 };
 
-// Add implementation of member functions here
+//Non default heap of binary tree
+template <typename T, typename PComparator> 
+Heap<T,PComparator>::Heap(int m, PComparator c){
+  mAry= m;
+  comp = c;
+}
+
+template <typename T, typename PComparator>
+Heap<T,PComparator>::~Heap(){}
+
+template<typename T, typename PComparator>
+void Heap<T,PComparator>::heapify(int indx){
+  int value = indx;
+  int bestValue = indx;
+//iterate through possible child nodes
+  for(int i =1;i <= mAry;i++){
+    //possible child 
+    unsigned int loc = mAry*indx+i;
+    if(loc < heapUsed.size() && comp(heapUsed[loc], heapUsed[value])){
+      value = loc;
+      if(comp(heapUsed[value], heapUsed[bestValue])){
+        bestValue = value;
+      }
+    }
+  }
+
+  if(bestValue != indx){
+    std::swap(heapUsed[indx], heapUsed[bestValue]);
+    heapify(bestValue);
+  }
+}
+
+template<typename T, typename PComparator>
+void Heap<T,PComparator>::trickleUp(int loc){
+  //General form for finding parent
+  int parent = (loc-1)/mAry;
+  
+  if(comp(heapUsed[loc], heapUsed[parent])){
+    std::swap(heapUsed[loc],heapUsed[parent]);
+    trickleUp(parent);
+  }
+
+  //code used from CSCI104 slides, swapping until in correct place
+  while(parent >= 1 && comp(heapUsed[loc], heapUsed[parent])){
+    std::swap(heapUsed[loc],heapUsed[parent]);
+    loc = parent;
+    parent = (loc-1)/mAry;
+  }
+
+}
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::push(const T& item){
+  //code used from CSCI104 slides, push item and maintain heap property
+  heapUsed.push_back(item);
+  trickleUp(heapUsed.size()-1);
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const{
+  return (heapUsed.size());
+}
 
 
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const{
+  if(heapUsed.size() == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
 template <typename T, typename PComparator>
@@ -81,14 +151,10 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+    throw std::underflow_error("Heap is empty!");
   }
-  // If we get here we know the heap has at least 1 item
-  // Add code to return the top element
-
-
-
+  //top item is at 0-index
+  return heapUsed[0];
 }
 
 
@@ -101,37 +167,13 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+  throw std::underflow_error("Heap is empty!");
   }
-
-template <typename T>
-void Heap::push(const T& item){
-  std::vector<T>::push_back(item);
+  //code used from CSCI104 slides, last item in vector to top, retain heap property
+  heapUsed[0] = heapUsed.back();
+  heapUsed.pop_back();
+  heapify(0);
 }
-
-template <typename T>
-bool Heap::empty() const{
-  return (std::vector::size()==0);
-}
-
-template <typename T>
-size_t Heap::size() const{
-  return (std::vector::size());
-}
-
-template <typename T>
-T const & Heap::top() const{
-  if(this->empty()){
-    throw std::underflow_error("Heap is empty!");
-  }
-  else{
-    return (std::vector<T>::at(0));
-  }
-}
-}
-
-
 
 #endif
 
--- logicsim/event.h
+++ logicsim/event.h
@@ -9,8 +9,11 @@ struct Event
     char state;
 };
 
+//Functor to compare time
 typedef struct EventLess {
-        //write the operator() required to make this a functor that compares Events by time
+    bool operator()(Event* one, Event* two){
+    return (one->time < two->time);
+  }
 } EventLess;
 	
 #endif
