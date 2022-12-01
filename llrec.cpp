391472c66f549b2731a502acba25e652d4ebbc6d
Leonel Herrera <leoherre@usc.edu>
2022-12-01 07:24:44 +0000

    Adding fixed code for regrade

--- llrec.cpp
+++ llrec.cpp
@@ -4,23 +4,25 @@
 // Provide your implementation of llpivot below
 //*********************************************
 void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot){
-  if(head == NULL){
+  if(head == nullptr){
+    smaller = nullptr;
+    larger = nullptr;
     return;
   }
-  else{
-    llpivot(head->next,smaller,larger,pivot);
-    //adds value grater than pivot to larger list 
-    if((head->val) > pivot){
-      head->next = larger;
-      larger = head; 
-    }
-    //adds value less than to smaller list 
-    else if(head->val <= pivot){
-      head->next = smaller;
-      smaller = head;
-    }
 
-    head = NULL;
+  if (head->val <= pivot) { 
+    smaller = head; 
+    head = head->next; 
+    smaller->next = nullptr;
+    llpivot(head, smaller->next, larger, pivot);
   }
+
+  else if (head->val > pivot){ 
+    larger = head; 
+    head = head->next; 
+    larger->next = nullptr;
+    llpivot(head, smaller, larger->next, pivot);
+  }
+  head = nullptr;
 }
 
